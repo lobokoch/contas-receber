0884bbc357a69a93d4d1f1de099d590142bea777
Márcio Koch <lobokoch@gmail.com>
2019-09-03 06:36:36 -0300

    Ajuste no SQL

--- modules/app/src/main/resources/db/migration/financeiro/contas_receber/PostgreSql/PERSONAL/V2__bootstrap_plano_contas.sql
+++ modules/app/src/main/resources/db/migration/financeiro/contas_receber/PostgreSql/PERSONAL/V2__bootstrap_plano_contas.sql
@@ -23,20 +23,20 @@ INSERT INTO plano_conta (id,codigo,descricao,tipo_financeiro,tipo_receita_despes
 ,('a425a8ba-1012-43b8-a43a-9ea9276a7b45','2.5.2','Diversos','RECEITA',NULL,'0dbdbdeb-2780-4789-beee-3a0f034c588b',true,false)
 ON CONFLICT ON CONSTRAINT pk_plano_conta_id DO NOTHING;
 
-INSERT INTO plano_conta (id,codigo,descricao,tipo_financeiro,tipo_receita_despesa,plano_conta_pai,ativo,created_by,created_date,last_modified_by,last_modified_date) VALUES 
-('a9e6be7f-c79c-46b8-b935-ae0ff4762bfa','2.3.2','Terreno','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true,'lobokoch@gmail.com','2019-07-25 21:14:17.927','lobokoch@gmail.com','2019-07-25 21:14:17.927')
-,('edfab525-ed6e-4d93-9e12-196a9946c6ef','2.3.3','Casa','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true,'lobokoch@gmail.com','2019-08-22 06:49:05.742','lobokoch@gmail.com','2019-08-22 06:49:05.742')
-,('7be58cf0-12f4-487f-9cac-1ea138749a63','2.3.4','Apartamento','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true,'lobokoch@gmail.com','2019-08-22 06:49:13.912','lobokoch@gmail.com','2019-08-22 06:49:13.912')
-,('23dce40f-77b5-4eda-9052-aa79617d076f','2.3.5','Automóvel','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true,'lobokoch@gmail.com','2019-08-22 06:49:26.288','lobokoch@gmail.com','2019-08-22 06:49:26.288')
-,('649dba27-bd83-4d09-a31f-ec52843d2031','2.3.6','Bicicleta','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true,'lobokoch@gmail.com','2019-08-22 06:49:33.463','lobokoch@gmail.com','2019-08-22 06:49:33.463')
-,('f095da7f-5779-4f77-afdf-86e672d35048','2.3.7','Motocicleta','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true,'lobokoch@gmail.com','2019-08-22 06:50:11.725','lobokoch@gmail.com','2019-08-22 06:50:11.725')
-,('45224d52-bc47-4f7d-86cb-8bdec214a701','2.3.8','Eletrodoméstico','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true,'lobokoch@gmail.com','2019-08-22 06:50:22.461','lobokoch@gmail.com','2019-08-22 06:50:22.461')
-,('38ad7042-abbc-4708-abd0-ccaf7b344f41','2.3.9','Imóvel','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true,'lobokoch@gmail.com','2019-08-22 06:56:04.078','lobokoch@gmail.com','2019-08-22 06:56:04.078')
-,('23de3c57-1c7f-4ac3-a4ab-6192ddbdd39a','2.6','REEMBOLSOS','RECEITA',NULL,'1ea1d30c-83e2-4f8f-8c39-ee53ef0d79fe',true,'lobokoch@gmail.com','2019-08-12 20:41:00.195','lobokoch@gmail.com','2019-08-12 20:41:00.195')
-,('68fef6de-142a-42d9-8201-4ccf2facf685','2.6.1','Unimed','RECEITA',NULL,'23de3c57-1c7f-4ac3-a4ab-6192ddbdd39a',true,'lobokoch@gmail.com','2019-08-12 20:41:49.470','lobokoch@gmail.com','2019-08-12 20:41:49.470')
+INSERT INTO plano_conta (id,codigo,descricao,tipo_financeiro,tipo_receita_despesa,plano_conta_pai,ativo) VALUES 
+('a9e6be7f-c79c-46b8-b935-ae0ff4762bfa','2.3.2','Terreno','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true)
+,('edfab525-ed6e-4d93-9e12-196a9946c6ef','2.3.3','Casa','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true)
+,('7be58cf0-12f4-487f-9cac-1ea138749a63','2.3.4','Apartamento','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true)
+,('23dce40f-77b5-4eda-9052-aa79617d076f','2.3.5','Automóvel','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true)
+,('649dba27-bd83-4d09-a31f-ec52843d2031','2.3.6','Bicicleta','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true)
+,('f095da7f-5779-4f77-afdf-86e672d35048','2.3.7','Motocicleta','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true)
+,('45224d52-bc47-4f7d-86cb-8bdec214a701','2.3.8','Eletrodoméstico','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true)
+,('38ad7042-abbc-4708-abd0-ccaf7b344f41','2.3.9','Imóvel','RECEITA',NULL,'e2516f3d-6f29-4933-88ac-c4f697c5222b',true)
+,('23de3c57-1c7f-4ac3-a4ab-6192ddbdd39a','2.6','REEMBOLSOS','RECEITA',NULL,'1ea1d30c-83e2-4f8f-8c39-ee53ef0d79fe',true)
+,('68fef6de-142a-42d9-8201-4ccf2facf685','2.6.1','Unimed','RECEITA',NULL,'23de3c57-1c7f-4ac3-a4ab-6192ddbdd39a',true)
 ON CONFLICT ON CONSTRAINT pk_plano_conta_id DO NOTHING;
 
-INSERT INTO plano_conta (id,codigo,descricao,tipo_financeiro,tipo_receita_despesa,plano_conta_pai,ativo,created_by,created_date,last_modified_by,last_modified_date) VALUES 
-('c05676c7-5467-4dd0-905a-4318c4432f41','2.7','AJUSTE DE SALDO','RECEITA',NULL,'1ea1d30c-83e2-4f8f-8c39-ee53ef0d79fe',true,'lobokoch@gmail.com','2019-08-12 21:18:58.481','lobokoch@gmail.com','2019-08-12 21:18:58.481')
-,('48526fc3-a9c5-4d16-8631-f7dc826b110c','2.7.1','Ajuste de Saldo','RECEITA',NULL,'c05676c7-5467-4dd0-905a-4318c4432f41',true,'lobokoch@gmail.com','2019-08-12 21:20:04.294','lobokoch@gmail.com','2019-08-12 21:20:04.294')
+INSERT INTO plano_conta (id,codigo,descricao,tipo_financeiro,tipo_receita_despesa,plano_conta_pai,ativo) VALUES 
+('c05676c7-5467-4dd0-905a-4318c4432f41','2.7','AJUSTE DE SALDO','RECEITA',NULL,'1ea1d30c-83e2-4f8f-8c39-ee53ef0d79fe',true)
+,('48526fc3-a9c5-4d16-8631-f7dc826b110c','2.7.1','Ajuste de Saldo','RECEITA',NULL,'c05676c7-5467-4dd0-905a-4318c4432f41',true)
 ON CONFLICT ON CONSTRAINT pk_plano_conta_id DO NOTHING;
